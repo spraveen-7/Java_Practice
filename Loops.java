d374077d7c08132e28204f99ebeaa7a32a1e4cb0
Praveen <Praveen@gmail.com>
2024-10-29 10:54:15 +0530

    Added Break and Continue

--- Loops.java
+++ Loops.java
@@ -3,6 +3,9 @@ public class Loops {
         int i=0;
         for (i=0; i < 10; i++) {
             System.out.print("Git  ");
+            if (i!=5 && i<8 && i>0 && i==4) {
+                break;
+            }
         }
         while (i>0) {
             System.out.println("While");
@@ -10,6 +13,10 @@ public class Loops {
         }
         System.out.println(i);
         do {
+            if (i>5) {
+                i++;
+                continue;
+            }
             System.out.println("dowhile");
             i++;
         } while (i!=10);
